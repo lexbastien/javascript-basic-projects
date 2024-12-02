f9ec29e258ede29a2be7ea5dce4ab6431a288e13
lexbastien <172320375+lexbastien@users.noreply.github.com>
2024-12-02 07:19:17 -0300

    05-sidebar, 06-modal, 07-questions

---
+++ 06-modal/app.js
@@ -0,0 +1,11 @@
+const modalBtn = document.querySelector('.modal-btn');
+const overlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', function(){
+    overlay.classList.add('open-modal');
+});
+
+closeBtn.addEventListener('click', function(){
+    overlay.classList.remove('open-modal');
+})
